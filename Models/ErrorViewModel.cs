4390d63c887f344aac9342ec0b96b36487240db8
Dean Pitts <Deano.2003.Pitts@gmail.com>
2025-10-03 09:47:44 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV6212_ICE2_ST10449407.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/TableStorageService.cs
@@ -0,0 +1,39 @@
+﻿
+using Azure;
+using Azure.Data.Tables;
+using Azure.Identity;
+using CLDV6212_ICE2_ST10449407.Models;
+
+namespace CLDV6212_ICE2_ST10449407.Services
+{
+    public class TableStorageService
+    {
+
+        private readonly TableClient _tableClient;
+
+        public TableStorageService(string connectionString, string tableName)
+        {
+            _tableClient = new TableClient(connectionString, tableName);
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _tableClient.UpsertEntityAsync(product, TableUpdateMode.Replace);
+        }
+
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            var results = _tableClient.QueryAsync<Product>();
+
+            var entities = new List<Product>();
+            await foreach (var item in results)
+            {
+                entities.Add(item);
+            }
+            return entities;
+        }
+
+    }
+}
